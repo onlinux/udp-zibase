a02125707182ac0479612972e326b486f80e2260
eric <onlinux@free.fr>
2015-12-19 16:28:08 +0100

    update app.js readUIntBE not available on  node version <12

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ client.on('listening', function(){
 
 client.on("message", function (msg, rinfo) {
     var date = moment();
-    var ip = msg.readUInt32BE(38, 4);
+    var ip = msg.readUInt32BE(38, 4); //msg.readUIntBE if node -v > 11
     zibaseIp = num2dot(ip);
 
     console.log(date.format(dateFormat) + ' '+ msg.toString(undefined, 6,12 )+ " " + msg.toString(undefined, 22,34 ) + ' IP is  ' + zibaseIp);
